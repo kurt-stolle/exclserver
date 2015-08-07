294e14f6e2fb97806648de71064bba2bc0bca354
Kurt Stolle <kurt@casualbananas.com>
2015-08-07 19:25:57 +0200

    Removed net opti

--- addon/lua/exclserver/util/sh_net.lua
+++
@@ -1,8 +0,0 @@
--- In most situations, out own serialization is a lot faster than the default.
-
-function net.WriteTable(tab)
-  net.WriteString(ES.Serialize(tab))
-end
-function net.ReadTable()
-  return ES.Deserialize(net.ReadString())
-end
